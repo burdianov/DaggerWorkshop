b274e20d5a31a5532e560144f0468970fc08b930
Lilian Burdianov <lilian.burdianov@gmail.com>
2017-02-10 17:30:56 +0400

    Add ApplicationComponent

--- app/src/main/java/com/crackncrunch/dagger2tutorial/di/GithubApplicationComponent.java
+++ app/src/main/java/com/crackncrunch/dagger2tutorial/di/GithubApplicationComponent.java
@@ -1,8 +1,16 @@
 package com.crackncrunch.dagger2tutorial.di;
 
+import com.crackncrunch.dagger2tutorial.network.GithubService;
+import com.squareup.picasso.Picasso;
+
+import dagger.Component;
+
 /**
  * Created by Lilian on 10-Feb-17.
  */
 
+@Component
 public interface GithubApplicationComponent {
+    Picasso getPicasso();
+    GithubService getGithubService();
 }
